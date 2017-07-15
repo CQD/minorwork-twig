84324e9fd31562f64f40606702da8dce4af816bf
CQD <thecqd@gmail.com>
2017-07-15 16:34:11 +0800

    Implement TwigView

---
+++ src/TwigView.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace MinorWork\View;
+
+use Twig_Loader_Filesystem as FileLoader;
+use Twig_Environment as Twig;
+
+class TwigView
+{
+    private $template = '';
+    private $params = [];
+    private $basePath = null;
+    private $twig = null;
+    private $twigOptions = [];
+
+    /**
+     * Prepare to render result
+     *
+     * The framework will call `toString()` to get rendered result.
+     *
+     * @param string $template
+     * @param string $params
+     */
+    public function prepare($template, $params = [])
+    {
+        $this->template = $template;
+        $this->params = $params;
+    }
+
+    /**
+     * Set base path for Twig to load templates
+     *
+     * @params string $basePath
+     */
+    public function basePath($basePath)
+    {
+        $this->basePath = $basePath;
+        $this->twig = null;
+    }
+
+    /**
+     * Set options to pass to twig
+     *
+     * @param array $options
+     */
+    public function options($options)
+    {
+        $this->twigOptions = $options;
+        $this->twig = null;
+    }
+
+    /**
+     * Actually render the template
+     * @return string rendered result
+     */
+    public function __toString()
+    {
+        $this->twig = $this->twig ?: new Twig(new FileLoader($this->basePath), $this->twigOptions);
+
+        return $this->twig->render($this->template, $this->params);
+    }
+}
---
+++ tests/src/TwigViewTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MinorWork\View;
+
+use PHPUnit\Framework\TestCase;
+use MinorWork\App;
+
+class TwigViewTest extends TestCase
+{
+    public function testTwig()
+    {
+        $app = new App();
+        $app->view = '\MinorWork\View\TwigView';
+
+        $app->view->basePath(dirname(__FILE__));
+        $app->view->prepare('test.twig', ['job' => 'Doctor', 'not_my_job' => 'magician']);
+
+        $this->assertEquals("I am a Doctor, not a magician!\n", (string) $app->view);
+    }
+}
